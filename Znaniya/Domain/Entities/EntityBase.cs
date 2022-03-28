45392d2154fcfbb0bb55ab4a8e694d23f2486af1
Дмитрий Кулешов <lbvf2013lbvf2013@gmail.com>
2022-03-28 14:22:58 +0500

    фикс

--- Znaniya/Domain/Entities/EntityBase.cs
+++ Znaniya/Domain/Entities/EntityBase.cs
@@ -4,15 +4,13 @@ namespace Znaniya.Domain.Entities
 {
     public abstract class EntityBase
     {
-        protected EntityBase() => DateAdded = DateTime.UtcNow; 
-
         [Required]
         public Guid Id { get; set; }
 
         [Display(Name = "Название (заголовок)")]
         public virtual string Title { get; set; }
 
-        [Display(Name = "Краткое описание0")]
+        [Display(Name = "Краткое описание")]
         public virtual string Subtitle { get; set; }
 
         [Display(Name = "Полное описание")]
@@ -24,7 +22,13 @@ namespace Znaniya.Domain.Entities
         [Display(Name = "SEO метатег Title")]
         public string MetaTitle { get; set; }
 
+        [Display(Name = "SEO метатег Description")]
+        public string MetaDescription { get; set; }
+
+        [Display(Name = "SEO метатег Keywords")]
+        public string MetaKeywords { get; set; }
+
         [DataType(DataType.Time)]
-        public DateTime DateAdded {get; set;}
+        public DateTime DateAdded { get; set; }
     }
 }
