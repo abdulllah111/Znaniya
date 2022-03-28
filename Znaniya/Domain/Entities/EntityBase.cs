0dbb550c06bb32f81abbde62190b050d681a27b3
Дмитрий Кулешов <lbvf2013lbvf2013@gmail.com>
2022-03-28 10:59:00 +0500

    Модели

---
+++ Znaniya/Domain/Entities/EntityBase.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Znaniya.Domain.Entities
+{
+    public abstract class EntityBase
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Название (заголовок)")]
+        public virtual string Title { get; set; }
+
+        [Display(Name = "Краткое описание0")]
+        public virtual string Subtitle { get; set; }
+
+        [Display(Name = "Полное описание")]
+        public virtual string Text { get; set; }
+
+        [Display(Name = "Титульная картинка")]
+        public virtual string TitleImagePath { get; set; }
+
+        [Display(Name = "SEO метатег Title")]
+        public string MetaTitle { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime DateAdded {get; set;}
+    }
+}
