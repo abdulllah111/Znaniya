a9e2903ac02f435097d34c7fcaf296da72fdd12b
Абдул Темирбеков <nasimhamdamov2017@mail.ru>
2022-03-29 11:22:03 +0500

    adminpanel

--- Znaniya/Areas/Admin/Controllers/HomeController.cs
+++ Znaniya/Areas/Admin/Controllers/HomeController.cs
@@ -6,9 +6,16 @@ namespace Znaniya.Areas.Admin.Controllers
     [Area("Admin")]
     public class HomeController : Controller
     {
+        private readonly DataManager dataManager;
+
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(dataManager.ServiceItems.GetServiceItems());
         }
     }
 }
