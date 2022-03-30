a5e5f15fb7668e8dfcf71cf26b33e3a9696ece95
Абдул Темирбеков <nasimhamdamov2017@mail.ru>
2022-03-30 14:29:54 +0500

    BooksCRD

--- Znaniya/Areas/Admin/Controllers/ShelfsController.cs
+++ Znaniya/Areas/Admin/Controllers/ShelfsController.cs
@@ -22,8 +22,16 @@ namespace MyCompany.Areas.Admin.Controllers
         }
         public IActionResult Index(Guid id)
         {
-            var model = dataManager.Shelfs.GetShelfById(id);
-            model.Books = dataManager.Books.GetBookByShelfId(model.ShelfID);
+            //var model = dataManager.Shelfs.GetShelfById(id);
+            ////model.Books = from book in dataManager.Shelfs.GetShelves()
+            ////              select new Book { Text = genre., Value = genre.Id.ToString() };
+
+            //model.Books = dataManager.Books.GetBookByShelfId(model.ShelfID);
+
+            var model = new AllPositions {
+                Shelf = dataManager.Shelfs.GetShelfById(id),
+                Books = dataManager.Books.GetBookByShelfId(id)
+            };
             return View(model);
         }
         public IActionResult Edit(Guid id)
