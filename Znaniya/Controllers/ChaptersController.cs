68249df416123d4679ff03470575291f8e8f313e
Абдул Темирбеков <nasimhamdamov2017@mail.ru>
2022-04-04 12:09:51 +0500

    Viewforallusers

---
+++ Znaniya/Controllers/ChaptersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Znaniya.Domain;
+using Znaniya.Domain.Entities;
+
+namespace Znaniya.Controllers
+{
+    public class ChaptersController : Controller
+    {
+        private readonly DataManager dataManager;
+        private readonly IWebHostEnvironment hostingEnvironment;
+        public ChaptersController(DataManager dataManager, IWebHostEnvironment hostingEnvironment)
+        {
+            this.dataManager = dataManager;
+            this.hostingEnvironment = hostingEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details(Guid id)
+        {
+            var model = new AllPositions
+            {
+                Chapter = dataManager.Chapters.GetChapterById(id),
+                Pages = dataManager.Pages.GetPagesByChapterId(id)
+            };
+            return View(model);
+        }
+    }
+}
---
+++ Znaniya/Controllers/PagesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Znaniya.Domain;
+
+namespace Znaniya.Controllers
+{
+    public class PagesController : Controller
+    {
+        private readonly DataManager dataManager;
+        private readonly IWebHostEnvironment hostingEnvironment;
+        public PagesController(DataManager dataManager, IWebHostEnvironment hostingEnvironment)
+        {
+            this.dataManager = dataManager;
+            this.hostingEnvironment = hostingEnvironment;
+        }
+        public ActionResult Details(Guid id)
+        {
+            var model = dataManager.Pages.GetPageById(id);
+            return View(model);
+        }
+    }
+}
